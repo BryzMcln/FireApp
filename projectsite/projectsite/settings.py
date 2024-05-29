0a139ab29af0676962c52c5178f982aa8ff9b449
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-05-29 20:59:03 +0800

    allowed host 2

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-d@4a$whi@(x03p+^ccwzokc$xcs*)qez!1(p_bdyvq1ebk6!3e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["BrrZynx.pythonanywhere.com", '127.0.0.1']
 
 
 # Application definition
