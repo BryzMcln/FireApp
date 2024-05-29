0614ae575f53729b9fce8918af8ca9c67f562bc4
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-05-29 10:30:01 +0800

    list and chart view added

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from fire.models import Locations, Incident
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Locations
+    context_object_name = 'home'
+    template_name = "home.html"
+
+class ChartView(ListView):
+    template_name = 'chart.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self, *args, **kwargs):
+        pass
