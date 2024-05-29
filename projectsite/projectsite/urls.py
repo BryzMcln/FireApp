f2f851b04049548f3bd02a63dcb6fe979ca153c2
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-05-29 10:33:15 +0800

    urls update

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from fire.views import HomePageView, ChartView
+from fire import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path('', HomePageView.as_view(), name='home'),
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+
 ]
