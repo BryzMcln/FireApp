8b5c6730d87962d5b3eda6bde153a81efa1980be
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-05-31 15:02:59 +0800

    dynamic pie chart

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-from fire.models import Locations, Incident
 from django.db import connection
 from django.http import JsonResponse
 from django.db.models.functions import ExtractMonth
 from django.db.models import Count
 from datetime import datetime
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from fire.models import Locations, Incident
 
 class HomePageView(ListView):
     model = Locations
