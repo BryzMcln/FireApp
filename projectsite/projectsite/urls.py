0c7a676c5aac625fec04f23bba68564b8dd15c80
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-05-31 15:11:45 +0800

    dynamic pie chart 2

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-
-from fire.views import HomePageView,  ChartView
+from fire.views import HomePageView, ChartView, PieCountbySeverity
 from fire import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-        path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
+    path('chart/', PieCountbySeverity, name='chart'),
 
 ]
