6468f7ecca10bc5727d7b0fbd7d52ffdc598eaa2
BryzMcln <jhnbrypntjmcln@gmail.com>
2024-06-01 22:39:56 +0800

    charts

--- projectsite/fire/models.py
+++ projectsite/fire/models.py
@@ -1,14 +1,9 @@
 from django.db import models
-
-
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
-
     class Meta:
         abstract = True
-
-
 class Locations(BaseModel):
     name = models.CharField(max_length=150)
     latitude = models.DecimalField(
@@ -19,7 +14,8 @@ class Locations(BaseModel):
     city = models.CharField(max_length=150)  # can be in separate table
     country = models.CharField(max_length=150)  # can be in separate table
 
-
+    def __str__(self):
+        return self.name
 class Incident(BaseModel):
     SEVERITY_CHOICES = (
         ('Minor Fire', 'Minor Fire'),
@@ -31,7 +27,8 @@ class Incident(BaseModel):
     severity_level = models.CharField(max_length=45, choices=SEVERITY_CHOICES)
     description = models.CharField(max_length=250)
 
-
+    def __str__(self):
+        return self.description
 class FireStation(BaseModel):
     name = models.CharField(max_length=150)
     latitude = models.DecimalField(
@@ -42,7 +39,8 @@ class FireStation(BaseModel):
     city = models.CharField(max_length=150)  # can be in separate table
     country = models.CharField(max_length=150)  # can be in separate table
 
-
+    def __str__(self):
+        return self.name
 class Firefighters(BaseModel):
     XP_CHOICES = (
         ('Probationary Firefighter', 'Probationary Firefighter'),
@@ -57,18 +55,23 @@ class Firefighters(BaseModel):
     experience_level = models.CharField(max_length=150)
     station = models.CharField(
         max_length=45, null=True, blank=True, choices=XP_CHOICES)
-
-
+    
+    def __str__(self):
+        return self.name
 class FireTruck(BaseModel):
     truck_number = models.CharField(max_length=150)
     model = models.CharField(max_length=150)
     capacity = models.CharField(max_length=150)  # water
     station = models.ForeignKey(FireStation, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.truck_number
 class WeatherConditions(BaseModel):
     incident = models.ForeignKey(Incident, on_delete=models.CASCADE)
     temperature = models.DecimalField(max_digits=10, decimal_places=2)
     humidity = models.DecimalField(max_digits=10, decimal_places=2)
     wind_speed = models.DecimalField(max_digits=10, decimal_places=2)
     weather_description = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.weather_description
